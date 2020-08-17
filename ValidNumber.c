9b8d76f9c78961e38dcd577c523f15b4f73acd6b
Yul3n <yul3n.falx@protonmail.com>
2020-08-17 19:04:59 +0200

    Started fixing.

--- ValidNumber.c
+++ ValidNumber.c
@@ -1,12 +1,14 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <stdio.h>
+
 char *
 skipSpaces(char *s)
 {
 	while (*(s++) == ' ');
 	--s;
 	return s;
+
 }
 
 bool
@@ -16,32 +18,41 @@ isNumber(char *s)
 	s = skipSpaces(s);
 
 	if (*s == '-' || *s == '+') ++s;
-	while (*s != 0 && isdigit(*(s++))) i = 1;
-	--s;
-	if (*s == 0) return true;
-	if (*s == '.' && i) {
+	while (*s != 0 && isdigit(*(s++))) {
+		i = 1;
+		if (*s == 0) return true;
+
+	} --s;
+	if (*s == '.') {
 		++s;
+		if (*s == 0 && i) return true;
 		if (*s == 'e') return false;
 		while (*s != 0 && isdigit(*(s++)));
 		--s;
+
 	}
 	if (*s == 'e' && i) {
-		if (*(++s) == 0) return false;
+		++s;
+		s = skipSpaces(s);
+		if (*s == 0) return false;
+
 	}
 	if (*s == '-' || *s == '+') ++s;
 	if (s != 0) {
-		while (*s != 0 && isdigit(*(s++)));
-
-		if (*s == 0) return true;
+		while (*s != 0 && isdigit(*(s++)))
+			if (*s == 0) return true;
 		--s;
+
 	}
 	s = skipSpaces(s);
-	if (*s == 0) return true;
+	if (*s == 0 && i) return true;
 	return false;
-}
 
+}
 int
 main(int argc, char **argv)
 {
 	printf("%d\n", isNumber(argv[1]));
 }
+
+
